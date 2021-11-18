27b0637daafbb50c3aea7c660a348d2a3d1a20c4
Mahardikadwiputra27 <91409732+Mahardikadwiputra27@users.noreply.github.com>
2021-11-18 16:07:33 +0700

    Add files via upload

---
+++ mainMenu.js
@@ -0,0 +1,28 @@
+let menu = [
+    { Item: 'Burger', Price: 30.000, Type: 'Main Course' },
+    { Item: 'Chicken Original', Price: 15.000, Type: 'Main Course' },
+    { Item: 'Chicken Original', Price: 16.000, Type: 'Main Course' },
+    { Item: 'French Fries', Price: 10.000, Type: 'Sides' },
+    { Item: 'Chicken Burger', Price: 15.000, Type: 'Main Course' },
+    { Item: 'Rice', Price: 8.000, Type: 'Sides' },
+    { Item: 'Cola', Price: 8.000, Type: 'Beverage' },
+    { Item: 'Sweet Tea', Price: 8.000, Type: 'Beverage' },
+]
+
+
+function order(list, input) {
+    // console.log(list)
+    let output = []
+    for (let i = 0; i < list.length; i++) {
+        const element = list[i];
+
+    }
+
+}
+console.log(order(menu, order))
+let order = [
+    {
+        'French fries': 2
+    }
+
+]
