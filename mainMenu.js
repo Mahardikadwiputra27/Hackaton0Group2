fa770e7f8eb7dbb146585e6bf40d54117e596398
Mahardikadwiputra27 <91409732+Mahardikadwiputra27@users.noreply.github.com>
2021-11-19 12:56:25 +0700

    Update mainMenu.js

--- mainMenu.js
+++ mainMenu.js
@@ -1,3 +1,5 @@
+
+
 let list = [
     { Item: 'Beef Burger', Price: 30_000, Type: 'Main Course' },
     { Item: 'Chicken Original', Price: 15_000, Type: 'Main Course' },
@@ -8,6 +10,7 @@ let list = [
     { Item: 'Cola', Price: 8_000, Type: 'Beverage' },
     { Item: 'Lemon Tea', Price: 8_000, Type: 'Beverage' },
 ]
+
 let selection = [
     { Item: 'French Fries' },
     { Item: 'Rice' },
@@ -15,25 +18,50 @@ let selection = [
     { Item: 'Lemon Tea' },
     { Item: 'Lemon Tea' },
     { Item: 'Beef Burger' },
-    { Item: 'Beef Burger' },
+    
 ]
 
-function pesan(menu, order) {
-    let output = 0
+function cartVisual(menu, order)     {
+    let output = []
+
     for (let i = 0; i < menu.length; i++) {
         for (let j = 0; j < order.length; j++) {
-
-            if (menu[i].Item === order[j].Item) {
-                output += menu[i].Price
-
-
+            if (order[j].Item === menu[i].Item) {
+                if (!output[menu[i]]) {
+                    output.push(menu[0])
+                    output[menu[i]].Quantity = 1
+                    delete output[menu[i]].Type
+                }
+                else {
+                    output[menu[i]].Quantity++
+                }
             }
         }
     }
 
     return output
+
 }
-// total price
+console.log(list, selection)
+
+
+
+
+
+function pesan(menu, order) {
+    let output = {}
+    let totalHarga = 0
+    for (let i = 0; i < menu.length; i++) {
+        for (let j = 0; j < order.length; j++) {
+            if (menu[i].Item === order[j].Item) {
+                totalHarga += menu[i].Price
+            }
+        }
+    }
+    output.totalHarga = totalHarga
+    return output
+}
+// console.log(pesan(list,selection))
 
 function paket(menu, order) {
     let main = false
@@ -42,6 +70,9 @@ function paket(menu, order) {
     let mainCount = 0
     let sidesCount = 0
     let beverageCount = 0
+    let countPaket = 0
+    let result = {}
+    let diskonPaket = 10000
 
     let output = pesan(menu, order)
     for (let i = 0; i < menu.length; i++) {
@@ -70,9 +101,9 @@ function paket(menu, order) {
             }
 
 
-
             if (main === true && sides === true && beverage === true) {
-                output -= 10000
+                output.totalHarga -= diskonPaket
+                countPaket++
                 main = false
                 sides = false
                 beverage = false
@@ -89,37 +120,90 @@ function paket(menu, order) {
                     beverageCount -= 1
                 }
             }
-
         }
     }
+    if (countPaket > 0) {
+        output.hargaAsli = output.totalHarga + (diskonPaket * countPaket)
+        output.message = `Selamat anda mendapatkan potongan sebanyak ${diskonPaket * countPaket} karena membeli paket ${countPaket} kali!`
+    }
     return output
-
 }
 // auto paket
+// console.log(paket(list, selection))
+
 function buy1(menu, order) {
     let output = paket(menu, order)
     let beefBurgercount = 0
+    let countbuy1 = 0
+    let promoBuy1 = 30000
     for (let i = 0; i < order.length; i++) {
         if (order[i].Item === 'Beef Burger') {
             beefBurgercount++
         }
     }
     if (beefBurgercount >= 2) {
-        output -= 30000
+        output.totalHarga -= promoBuy1
+        countbuy1++
     }
+
+
+    if (!output.message && countbuy1) {
+        output.message = `Selamat anda mendapatkan Beef Burger kedua tanpa biaya!`
+    }
+    else if (output.message && countbuy1) {
+        output.message += ` Anda juga mendapatkan Beef Burger kedua tanpa biaya!`
+    }
+    if (!output.hargaAsli) {
+        output.hargaAsli = output.totalHarga + promoBuy1
+    }
+
     return output
 }
 // buy1 get 1 burger
+// console.log(buy1(list, selection))
+
 function cashback(menu, order) {
     let output = buy1(menu, order)
+    let cashback1 = 0
+    let cashback2 = 0
+    let cashback1cut = 10000
+    let cashback2cut = 25000
 
-    if (output >= 40000 && output < 85000) {
-        output -= 10000
+    if (output.totalHarga >= 40000 && output.totalHarga < 85000) {
+        output.totalHarga -= cashback1cut
+        cashback1++
     }
-    else if (output >= 85000) {
-        output -= 25000
+    else if (output.totalHarga >= 85000) {
+        output.totalHarga -= cashback2cut
+        cashback2++
+    }
+    if (!output.message && cashback1cut) {
+        output.message = `Selamat anda mendapatkan cashback sebanyak ${cashback1cut} karena transaksi minimal 40000!`
+    }
+    else if (!output.message && cashback2cut) {
+        output.message = `Selamat anda mendapatkan cashback sebanyak ${cashback2cut} karena transaksi minimal 85000!`
+    }
+    else if (output.message) {
+        if (cashback1cut) {
+            output.message += ` Ada juga potongan cashback berjumlah ${cashback1cut} dengan transaksi minimal 40000!`
+        }
+        else if (cashback2cut) {
+            output.message += ` Ada juga potongan cashback berjumlah ${cashback2cut} dengan transaksi minimal 85000!`
+        }
+    }
+    if (!output.hargaAsli) {
+        if (cashback1cut) {
+            output.hargaAsli = output.totalHarga + cashback1cut
+        }
+        else if (cashback2cut) {
+            output.hargaAsli = output.totalHarga + cashback2cut
+        }
     }
     return output
 }
 console.log(cashback(list, selection))
+// cashback 40k/ 85k
+
+
+
 // cashback 40k/ 85k
