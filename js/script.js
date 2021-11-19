fa415bbd0b0d126b8fd1b78952f659f82ec2737a
rkrfld <rkrfld@gmail.com>
2021-11-19 14:54:17 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 }
@@ -9,22 +9,22 @@ menu.onclick = () =>{
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header .navbar a');
 
-window.onscroll = () =>{
+window.onscroll = () => {
 
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
 
-  section.forEach(sec =>{
+  section.forEach(sec => {
 
     let top = window.scrollY;
     let height = sec.offsetHeight;
     let offset = sec.offsetTop - 150;
     let id = sec.getAttribute('id');
 
-    if(top >= offset && top < offset + height){
-      navLinks.forEach(links =>{
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
         links.classList.remove('active');
-        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+        document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
       });
     };
 
@@ -51,7 +51,7 @@ var swiper = new Swiper(".home-slider", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  loop:true,
+  loop: true,
 });
 
 var swiper = new Swiper(".review-slider", {
@@ -61,10 +61,10 @@ var swiper = new Swiper(".review-slider", {
     delay: 7500,
     disableOnInteraction: false,
   },
-  loop:true,
+  loop: true,
   breakpoints: {
     0: {
-        slidesPerView: 1,
+      slidesPerView: 1,
     },
     640: {
       slidesPerView: 2,
@@ -78,12 +78,216 @@ var swiper = new Swiper(".review-slider", {
   },
 });
 
-function loader(){
+function loader() {
   document.querySelector('.loader-container').classList.add('fade-out');
 }
 
-function fadeOut(){
+function fadeOut() {
   setInterval(loader, 3000);
 }
 
 window.onload = fadeOut;
+
+// ************************************************
+// Shopping Cart API
+// ************************************************
+
+var shoppingCart = (function () {
+  cart = [];
+
+  // Constructor
+  function Item(name, price, count) {
+    this.name = name;
+    this.price = price;
+    this.count = count;
+  }
+
+  function saveCart() {
+    sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+  }
+
+  function loadCart() {
+    cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+  }
+  if (sessionStorage.getItem("shoppingCart") != null) {
+    loadCart();
+  }
+
+  var obj = {};
+
+  // Add to cart
+  obj.addItemToCart = function (name, price, count) {
+    for (var item in cart) {
+      if (cart[item].name === name) {
+        cart[item].count++;
+        saveCart();
+        return;
+      }
+    }
+    var item = new Item(name, price, count);
+    cart.push(item);
+    saveCart();
+  }
+  // Set count from item
+  obj.setCountForItem = function (name, count) {
+    for (var i in cart) {
+      if (cart[i].name === name) {
+        cart[i].count = count;
+        break;
+      }
+    }
+  };
+  // Remove item from cart
+  obj.removeItemFromCart = function (name) {
+    for (var item in cart) {
+      if (cart[item].name === name) {
+        cart[item].count--;
+        if (cart[item].count === 0) {
+          cart.splice(item, 1);
+        }
+        break;
+      }
+    }
+    saveCart();
+  }
+
+  // Remove all items from cart
+  obj.removeItemFromCartAll = function (name) {
+    for (var item in cart) {
+      if (cart[item].name === name) {
+        cart.splice(item, 1);
+        break;
+      }
+    }
+    saveCart();
+  }
+
+  // Clear cart
+  obj.clearCart = function () {
+    cart = [];
+    saveCart();
+  }
+
+  // Count cart 
+  obj.totalCount = function () {
+    var totalCount = 0;
+    for (var item in cart) {
+      totalCount += cart[item].count;
+    }
+    return totalCount;
+  }
+
+  // Total cart
+  obj.totalCart = function () {
+    var totalCart = 0;
+    for (var item in cart) {
+      totalCart += cart[item].price * cart[item].count;
+    }
+    return Number(totalCart.toFixed(2));
+  }
+
+  // List cart
+  obj.listCart = function () {
+    var cartCopy = [];
+    for (i in cart) {
+      item = cart[i];
+      itemCopy = {};
+      for (p in item) {
+        itemCopy[p] = item[p];
+
+      }
+      itemCopy.total = Number(item.price * item.count).toFixed(2);
+      cartCopy.push(itemCopy)
+    }
+    return cartCopy;
+  }
+
+  // cart : Array
+  // Item : Object/Class
+  // addItemToCart : Function
+  // removeItemFromCart : Function
+  // removeItemFromCartAll : Function
+  // clearCart : Function
+  // countCart : Function
+  // totalCart : Function
+  // listCart : Function
+  // saveCart : Function
+  // loadCart : Function
+  return obj;
+})();
+
+
+// *****************************************
+// Triggers / Events
+// ***************************************** 
+// Add item
+document.querySelectorAll(".add-to-cart").forEach((item) => {
+  item.addEventListener("click", (event) => {
+        let name = item.getAttribute('data-name');
+    let price = Number(item.getAttribute('data-price'));
+    shoppingCart.addItemToCart(name, price, 1);
+    displayCart();
+  })
+})
+
+// Clear items
+document.querySelector(".clear-cart").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    shoppingCart.clearCart();
+    displayCart();
+  })
+})
+
+
+function displayCart() {
+  var cartArray = shoppingCart.listCart();
+  var output = "";
+  for (var i in cartArray) {
+    output += "<tr>"
+      + "<td>" + cartArray[i].name + "</td>"
+      + "<td>(" + cartArray[i].price + ")</td>"
+      + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+      + "<td>" + cartArray[i].total + "</td>"
+      + "</tr>";
+  }
+  document.querySelector(".show-cart").innerHTML = output;
+  document.querySelector(".total-cart").innerHTML = shoppingCart.totalCart();
+  document.querySelector(".total-count").innerHTML = shoppingCart.totalCount();
+}
+
+
+// Delete item button
+document.querySelector(".delete-item").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    let name = item.getAttribute('data-name')
+    shoppingCart.removeItemFromCartAll(name);
+    displayCart();
+  })
+})
+
+document.querySelector(".minus-item").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    let name = item.getAttribute('data-name')
+    shoppingCart.removeItemFromCart(name);
+    displayCart();
+  })
+})
+
+document.querySelector(".plus-item").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    let name = item.getAttribute('data-name')
+    shoppingCart.addItemToCart(name);
+    displayCart();
+  })
+})
+displayCart();
+document.querySelector(".item-cart").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    let name = item.getAttribute('data-name');
+    let count = Number(item.getAttribute("value"));
+    shoppingCart.setCountForItem(name, count);
+    displayCart();
+  })
+})
+
+displayCart()
